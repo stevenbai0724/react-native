d138401ac0c080dad54fdb026de3ba6a978117bb
stevenbai0724 <stevenbai0724@gmail.com>
2021-09-13 16:17:59 -0400

    9/13/21

---
+++ App.js
@@ -0,0 +1,47 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/task.js';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+
+      <View style = {styles.tasksWrapper}>
+
+        <Text style = {styles.sectionTitle}>Today's tasks</Text>
+
+        <View style = {styles.item}>
+
+          {/* Tasks */}
+
+          <Task text = {'Task 1'}/>
+          <Task text = {'Task 2'}/>
+        </View>
+
+      </View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E8EAED',
+
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  item: {
+
+  },
+
+});
